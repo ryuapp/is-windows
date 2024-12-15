c0caac08c4edb9fb8a5dd4a73e5a6e34d8ab2333
ryu <114303361+ryuapp@users.noreply.github.com>
2024-12-15 14:21:10 +0900

    feat: support for browser

--- main.ts
+++ main.ts
@@ -1,11 +1,20 @@
 // deno-lint-ignore no-explicit-any
-const { Deno, process } = globalThis as any;
-
-// TODO: Support for Browser
+const { Deno, process, window } = globalThis as any;
+/**
+ * Returns true if the current platform is Windows.
+ * Returns false if it is not Windows or cannot be determined.
+ *
+ * @example
+ * ```ts
+ * console.log(isWindows());
+ * ```
+ */
 export function isWindows(): boolean {
   return typeof Deno !== "undefined"
     ? Deno.build.os === "windows"
     : typeof process !== "undefined"
     ? process.platform === "win32"
+    : window !== "undefined"
+    ? window.navigator.userAgent.includes("Windows")
     : false;
 }
